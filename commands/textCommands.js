656b9ab295b1bdc3ec73f06eacb8b248a5853ab3
cannedyeti <cpotebnya@gmail.com>
2022-04-05 18:57:04 -0700

    Added version to help command

--- commands/textCommands.js
+++ commands/textCommands.js
@@ -140,6 +140,7 @@ const help = (msg) => {
         { name: "!mastery [summoner name]", value: "Bot will reply with the summoners top 3 champions." },
         { name: "!milkman [@discord name]", value: "The milkman will fuck their wife." },
         { name: "!teams", value: "Bot will create a message 10 users must react to within one minute. Once 10 users react, the bot will generate and post teams." },
+        { name: "!version", value: "Display version and latest commit." },
     )
     .setImage('https://cdn.discordapp.com/attachments/866869860319232022/941456015826759761/812DC7BE-FA2C-4D82-9074-79789A4145A3.jpg')
     .setFooter({ text: "Nice cock bro", iconURL: "https://cdn.discordapp.com/emojis/592482696920432652.webp?size=96&quality=lossless"});
