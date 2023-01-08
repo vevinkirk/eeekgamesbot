a607bc6db3d593381d7c92240b6a2e0b0c27a1b6
Alexander Becker <alexbecker@Alexanders-MacBook-Pro-2.local>
2023-01-08 00:19:56 -0800

    Updated help command photo

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,34 @@
-const { SlashCommandBuilder, EmbedBuilder  } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
-const help =  new EmbedBuilder()
-    .setTitle("Eeek games bot commands")
-    .setDescription("List of commands available with Eeek games bot")
-    .setAuthor({ name: 'A VirkCorn Original', iconURL: 'https://cdn.discordapp.com/emojis/660667449620037674.gif?size=96&quality=lossless', url: 'https://github.com/vevinkirk/eeekgamesbot' })
-    .addFields(
-        { name: "\u200B", value: "`teams` - Create randomly generated teams. \n `mastery` - Bot will reply with the summoners top 3 champions. \n `milkman` - The milkman will fuck their wife. \n `version` - Shows current bot version and latest commit. \n ", inline: true },
-    )
-    .setImage('https://cdn.discordapp.com/attachments/866869860319232022/941456015826759761/812DC7BE-FA2C-4D82-9074-79789A4145A3.jpg')
-    .setFooter({ text: "Nice cock bro", iconURL: "https://cdn.discordapp.com/emojis/592482696920432652.webp?size=96&quality=lossless"});
-
+const help = new EmbedBuilder()
+  .setTitle("Eeek games bot commands")
+  .setDescription("List of commands available with Eeek games bot")
+  .setAuthor({
+    name: "A VirkCorn Original",
+    iconURL:
+      "https://cdn.discordapp.com/emojis/660667449620037674.gif?size=96&quality=lossless",
+    url: "https://github.com/vevinkirk/eeekgamesbot",
+  })
+  .addFields({
+    name: "\u200B",
+    value:
+      "`teams` - Create randomly generated teams. \n `mastery` - Bot will reply with the summoners top 3 champions. \n `milkman` - The milkman will fuck their wife. \n `version` - Shows current bot version and latest commit. \n ",
+    inline: true,
+  })
+  .setImage(
+    "https://cdn.discordapp.com/attachments/278730910906581004/1061541169177296956/IMG_3183.png"
+  )
+  .setFooter({
+    text: "Nice cock bro",
+    iconURL:
+      "https://cdn.discordapp.com/emojis/592482696920432652.webp?size=96&quality=lossless",
+  });
 
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('Displays all available commands'),
-    async execute(interaction) {
-      await interaction.reply({embeds: [help]})
-    }
-  };
-  
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Displays all available commands"),
+  async execute(interaction) {
+    await interaction.reply({ embeds: [help] });
+  },
+};
